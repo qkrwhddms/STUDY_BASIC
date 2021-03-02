4205e36fa5c541cfa5b44d29f9732977c03f72cf
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-02 18:54:17 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_002.java
@@ -0,0 +1,16 @@
+package Study_chap01;
+
+
+public class Study_002 {
+
+	public static void main(String[] args) {
+		// 변수 선언과 변수대입 초기화
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		// 출력
+		System.out.println(result);
+
+	}
+
+}
