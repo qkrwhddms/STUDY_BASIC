0c83244000261f70599a1a686ac6125222457e08
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-03 10:11:53 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_003.java
@@ -0,0 +1,16 @@
+package Study_chap01;
+
+
+public class Study_003 {
+
+	public static void main(String[] args) {
+		
+		int value = 30;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+
+	}
+
+}
