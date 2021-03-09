6a5492458e7b89d7b592e3a969aad994d052218c
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-09 10:09:14 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_007.java
@@ -0,0 +1,20 @@
+package Study_chap01;
+
+
+public class Study_007 {
+
+	public static void main(String[] args) {
+		
+		int var1 = 0b1011;  // 2진수
+		int var2 = 013;     // 8진수
+		int var3 = 11;      // 10진수
+		int var4 = 0xB3;    // 16진수
+		
+		System.out.println("var1 : " + var1);  
+		System.out.println("var2 : " + var2);
+		System.out.println("var3 : " + var3);  
+		System.out.println("var4 : " + var4);  
+
+	}
+
+}
