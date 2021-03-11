c2d7df3a59b30b8aecba10198f16851f88bcf05e
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-11 10:07:31 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_009.java
@@ -0,0 +1,17 @@
+package Study_chap01;
+
+
+public class Study_009 {
+
+	public static void main(String[] args) {
+		
+		long var1 = 10;
+		long var2 = 20L;
+		long var3 = 20000000000L;
+
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	}
+
+}
