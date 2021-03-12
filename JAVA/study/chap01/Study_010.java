9ad1e0f4ddcbd873663d396e799532a4a95e4412
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-12 10:13:41 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_010.java
@@ -0,0 +1,27 @@
+package Study_chap01;
+
+
+public class Study_010 {
+
+	public static void main(String[] args) {
+		
+		char c1 = 'A';
+		char c2 = 65;
+		char c3 = '\u0041';
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		
+		char c4 = '가';
+		char c5 = 44032;
+		char c6 = '\uAC00';
+		
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);
+		
+		
+	}
+
+}
