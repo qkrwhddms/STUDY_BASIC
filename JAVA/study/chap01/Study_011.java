45c38cb881759f789c3b2e907fc324bb7803e7d2
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-15 18:42:56 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_011.java
@@ -0,0 +1,17 @@
+package Study_chap01;
+
+
+public class Study_011 {
+
+	public static void main(String[] args) {
+		
+		String name = "박종은";
+		String job = "프로그래머";
+		
+		System.out.println(name);
+		System.out.println(job);
+		
+
+	}
+
+}
