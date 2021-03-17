b24468cd96760b3067dc560862cbfa58097a08a6
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-17 10:08:40 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_013.java
@@ -0,0 +1,29 @@
+package Study_chap01;
+
+public class Study_013 {
+
+	public static void main(String[] args) {
+		
+		//float var1 = 3.14;
+		float var2 = 3.14F;
+		double var3 = 3.14;
+		
+		System.out.println(var2);
+		System.out.println(var3);
+
+		float var4 = 0.1234567890123456789F;
+		double var5 = 0.1234567890123456789;
+		
+		System.out.println(var4);
+		System.out.println(var5);
+		
+		double var6 = 3e6;
+		float var7 = 3e6F;
+		double var8 = 2e-3;
+		
+		System.out.println(var6);
+		System.out.println(var7);
+		System.out.println(var8);
+	}
+
+}
