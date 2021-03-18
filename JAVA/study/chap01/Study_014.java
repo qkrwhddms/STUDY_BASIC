837ad234b6120f8670bd7aee9c84cd1f11087f51
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-18 10:02:52 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_014.java
@@ -0,0 +1,17 @@
+package Study_chap01;
+
+public class Study_014 {
+
+	public static void main(String[] args) {
+		
+		boolean stop = false;
+		
+		if (stop) {
+			System.out.println("중지합니다.");
+		} else {
+			System.out.println("시작합니다.");
+		}
+
+	}
+
+}
