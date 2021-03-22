7e824f2ed98cf1b04744cc60f2cd57e8bcfec3ea
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-22 11:52:04 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_016.java
@@ -0,0 +1,21 @@
+package Study_chap01;
+
+public class Study_016 {
+
+	public static void main(String[] args) {
+		
+		int intValue = 44032;
+		char charValue = (char)intValue;
+		System.out.println(charValue);
+		
+		long longValue = 500;
+		intValue = (int)longValue;
+		System.out.println(intValue);
+		
+		double doubleValue = 3.14;
+		intValue = (int)doubleValue;
+		System.out.println(intValue);
+
+	}
+
+}
