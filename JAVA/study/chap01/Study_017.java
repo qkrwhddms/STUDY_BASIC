518ff1c675fea1d5a9f5eea5b1717df7da33d587
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-03-23 09:30:43 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_017.java
@@ -0,0 +1,16 @@
+package Study_chap01;
+
+public class Study_017 {
+
+	public static void main(String[] args) {
+		
+		byte result1 = 10 + 20;
+		
+		byte x = 10;
+		byte y = 20;
+		int result2 = x + y;
+
+		
+	}
+
+}
