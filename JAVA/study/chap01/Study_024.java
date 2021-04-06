4f448214c292a7947bcb1f481d32dca87e10c5b4
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-04-06 10:49:21 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_024.java
@@ -0,0 +1,16 @@
+package Study_chap01;
+
+public class Study_024 {
+
+	public static void main(String[] args) throws Exception {
+		
+		int KeyCode;
+		
+		while(true) {
+			KeyCode = System.in.read();
+			System.out.println("KeyCode : " + KeyCode);
+		}
+
+	}
+
+}
