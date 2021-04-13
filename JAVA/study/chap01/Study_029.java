60ff900cc91e650ed9131d7a9c581ba9434011db
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-04-13 10:48:08 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_029.java
@@ -0,0 +1,17 @@
+package Study_chap01;
+
+public class Study_029 {
+
+	public static void main(String[] args) {
+		
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;   
+		System.out.println(play);
+
+		play = !play;
+		System.out.println(play);
+	}
+
+}
