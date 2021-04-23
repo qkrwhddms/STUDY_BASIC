51508848194f733b476fbcee2376d1dd0d2026cc
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-04-23 10:15:40 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_036.java
@@ -0,0 +1,22 @@
+package Study_chap01;
+
+public class Study_036 {
+
+	public static void main(String[] args) {
+		
+		int result = 0;
+		
+		result += 10;
+		System.out.println("result = " + result);
+		result -= 5;
+		System.out.println("result = " + result);
+		result *= 3;
+		System.out.println("result = " + result);
+		result /= 5;
+		System.out.println("result = " + result);
+		result %= 3;
+		System.out.println("result = " + result);
+
+	}
+
+}
