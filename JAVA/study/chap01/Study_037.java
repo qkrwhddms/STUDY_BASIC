2e84047a01ed387be18899c2b45c51a949b4b295
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-04-26 22:38:26 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_037.java
@@ -0,0 +1,20 @@
+package Study_chap01;
+
+public class Study_037 {
+
+	public static void main(String[] args) {
+		
+		int score = 93;
+		
+		if(score >= 90) {
+			System.out.println("점수가 90보다 큽니다.");
+			System.out.println("등급은 A 입니다.");
+		}
+		
+		if(score < 90)
+			System.out.println("점수가 90보다 작습니다.");
+			System.out.println("등급은 B 입니다.");
+
+	}
+
+}
