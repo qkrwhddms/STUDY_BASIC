ebf2b90679661d4d18572b9a7993019dd84c4295
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-05-07 21:51:43 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_046.java
@@ -0,0 +1,18 @@
+package Study_chap01;
+
+public class Study_046 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		
+		int i;
+		for(i=1; i<=100; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1~" + (i-1) + "합 : " + sum);
+
+	}
+
+}
