47e9aa677b1514952eb7f45ba67494082029179a
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-05-10 19:27:01 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_047.java
@@ -0,0 +1,13 @@
+package Study_chap01;
+
+public class Study_047 {
+
+	public static void main(String[] args) {
+		
+		for (float x = 0.1f; x <= 1.0; x+=0.1f) {
+			System.out.println(x);
+		}
+
+	}
+
+}
