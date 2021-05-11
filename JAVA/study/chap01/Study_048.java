385217d61dc9c665d2f9e031123c56731a70f498
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-05-11 10:19:30 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_048.java
@@ -0,0 +1,16 @@
+package Study_chap01;
+
+public class Study_048 {
+
+	public static void main(String[] args) {
+		
+		for (int m = 2; m <= 9; m++) {
+			System.out.println("*** " + m + "단 ***");
+			for (int n = 1; n <= 9; n++) {
+				System.out.println(m + " x " + n + " = " + (m*n));
+			}
+		}
+
+	}
+
+}
