0ecc844dbe6ad6f2746b219879e78c7a7d7ee1b5
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-05-12 10:28:58 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_049.java
@@ -0,0 +1,15 @@
+package Study_chap01;
+
+public class Study_049 {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while(i<=10) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
