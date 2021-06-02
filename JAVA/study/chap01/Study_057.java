dd04882a82d5ca723253d62b47b9a821d3cfc2c1
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-06-03 00:11:13 +0900

    Add files via upload

---
+++ JAVA/study/chap01/Study_057.java
@@ -0,0 +1,33 @@
+package Study_chap01;
+
+public class Study_057 {
+
+	public static void main(String[] args) {
+		
+		String strVar1 = "박종은";
+		String strVar2 = "박종은";
+		
+		if(strVar1 == strVar2) {
+			System.out.println("strVar1과 strVar2는 참조가 같음");
+		}else {
+				System.out.println("strVar1과 strVar2는 참조가 다름");
+		}
+		if (strVar1.equals(strVar2)) {
+				System.out.println("strVar1과 strVar2는 문자열이 같음");
+		}
+
+		String strVar3 = new String("박종은");
+		String strVar4 = new String("박종은");
+		
+		if(strVar3 == strVar4) {
+			System.out.println("strVar3과 strVar4는 참조가 같음");
+		}else {
+				System.out.println("strVar3과 strVar4는 참조가 다름");
+		}
+		if (strVar3.equals(strVar4)) {
+				System.out.println("strVar3과 strVar4는 문자열이 같음");
+		}
+		
+	}
+
+}
