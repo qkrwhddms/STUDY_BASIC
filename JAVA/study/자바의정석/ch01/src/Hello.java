bdf2a364c098c369eff1e71c5b013f4bc572f827
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-06-18 01:01:40 +0900

    Add files via upload

---
+++ JAVA/study/자바의정석/ch01/src/Hello.java
@@ -0,0 +1,26 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// ctrl+shift+L   단축키 전체 목록보기
+		// ctrl+ +,-   폰트 크기 증가/감소
+		// ctrl+D   한 줄 삭제
+		// ctrl+alt+shift+down   행단위 복사
+		// alt+shift+A   멀티컬럼 편집
+		// alt+up, down   행 단위 이동
+		// ctrl+i   자동 들여쓰기
+		// ctrl+/   주석(토굴) -> 한줄 주석
+		// ctrl+space   자동완성
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("/*주석 아님*/");
+		System.out.println("// Hello,world.");
+		System.out.println("Hello,world.");
+		System.out.println("Hello,world.");
+		System.out.println("Hello,world.");
+		System.out.println("Hello,world.");
+		System.out.println("Hello,world.");
+	}
+ 
+}
