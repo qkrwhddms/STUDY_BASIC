8808e4a3cbe1732cd05a7d3d91ac34255592253d
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-07-12 10:39:26 +0900

    Add files via upload

---
+++ JAVA/study/자바의정석/ch02/src/PrintfEx1.java
@@ -0,0 +1,33 @@
+
+public class PrintfEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println(10.0/3);
+		
+//		System.out.printf("%d%n", 15);
+//		System.out.printf("%#o%n", 15);
+//		System.out.printf("%#x%n", 15);
+//		System.out.printf("%s",Integer.toBinaryString(15));
+		
+//		double f = 123.456789f;
+//		System.out.printf("%f%n",f);
+//		System.out.printf("%e%n",f);
+		
+		System.out.printf("[%5d]%n",1234567);
+		System.out.printf("[%-5d]%n",10);
+		System.out.printf("[%05d]%n",10);
+		
+		double d = 1.23456789;
+		System.out.printf("%.6f%n", d);
+		
+		System.out.printf("[%s]%n", "www.codechobo.com");
+		System.out.printf("[%20s]%n", "www.codechobo.com");
+		System.out.printf("[%-20s]%n", "www.codechobo.com");
+		System.out.printf("[%.10s]%n", "www.codechobo.com");
+
+		
+		
+	}
+
+}
