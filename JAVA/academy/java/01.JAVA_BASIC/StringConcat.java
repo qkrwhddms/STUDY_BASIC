7dcf07f90665fda1390bdb5416f40735750c34fe
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-10-11 19:33:19 +0900

    Add files via upload

---
+++ JAVA/academy/java/01.JAVA_BASIC/StringConcat.java
@@ -0,0 +1,23 @@
+
+public class StringConcat {
+
+	public static void main(String[] args) {
+		int year =2020;
+		int month=12;
+		int day = 9;
+		
+		String msg="오늘은 "+year+"년 "+month+"월 "+day+" 일 입니다";
+		System.out.println(msg);
+		
+		System.out.println("오늘은 "+year+"년 "+month+"월 "+day+" 일 입니다");
+		
+		System.out.println(1 + 2 + "김");
+		System.out.println(1 + (2 + "김"));
+		
+		
+		
+		
+
+	}
+
+}
