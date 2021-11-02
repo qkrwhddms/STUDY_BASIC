e73b6aa722becdcec3ce2cd0208362a9ddbfd583
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-11-02 10:26:28 +0900

    Add files via upload

---
+++ JAVA/academy/java/01.JAVA_BASIC/변수선언2.java
@@ -0,0 +1,20 @@
+
+public class 변수선언2 {
+
+	public static void main(String[] args) {
+		int score1;
+		/*
+		 * XXX
+		score cannot be resolved to a variable
+		 */
+		score1 = 100;
+		/*
+		 system cannot be resolved
+		 */
+		 System.out.println(score1);
+		 
+		
+
+	}
+
+}
