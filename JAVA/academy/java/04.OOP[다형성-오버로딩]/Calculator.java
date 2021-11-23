45c1c9652410043f87ade5036fc5c475f624098b
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-11-23 18:21:04 +0900

    Add files via upload

---
+++ JAVA/academy/java/04.OOP[다형성-오버로딩]/Calculator.java
@@ -0,0 +1,17 @@
+
+public class Calculator {
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+	public int add(int a,int b,int c) {
+		return a+b+c;
+	}
+	public int add(int a,int b,int c,int d) {
+		return a+b+c+d;
+	}
+	public int add(int a,int b,int c,int d,int e) {
+		return a+b+c+d+e;
+	}
+}
+
