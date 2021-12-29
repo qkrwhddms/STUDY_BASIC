3973eceae02cda57e1f331109fcab172053c9022
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2021-12-29 18:16:14 +0900

    Add files via upload

---
+++ JAVA/academy/java/08.배열[array]/IntArrayMain.java
@@ -0,0 +1,28 @@
+
+class IntArray{
+	int i0;
+	int i1;
+	int i2;
+	int i3;
+	int i4;
+}
+
+
+public class IntArrayMain {
+
+	public static void main(String[] args) {
+		IntArray intArray=new IntArray();
+		intArray.i0=11;
+		intArray.i1=22;
+		intArray.i2=33;
+		intArray.i3=44;
+		intArray.i4=55;
+		System.out.println(intArray.i0);
+		System.out.println(intArray.i1);
+		System.out.println(intArray.i2);
+		System.out.println(intArray.i3);
+		System.out.println(intArray.i4);
+		
+	}
+
+}
