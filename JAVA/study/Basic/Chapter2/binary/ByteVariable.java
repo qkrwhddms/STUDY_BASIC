3c2a3ea2ce77baba8f534271e364b5c46d005b99
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2022-01-18 15:04:04 +0900

    Add files via upload

---
+++ JAVA/study/Basic/Chapter2/binary/ByteVariable.java
@@ -0,0 +1,13 @@
+package binary;
+
+public class ByteVariable {
+
+	public static void main(String[] args) {
+		byte bData = -128;
+		System.out.println(bData);
+		
+		byte bData2 = 127;	// 128이상은 int로 사용
+		System.out.println(bData2);
+	}
+
+}
