de97042f4103d41ef86b00d7afd73a2c58d45f11
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2022-01-24 11:21:19 +0900

    Add files via upload

---
+++ JAVA/study/Basic/Chapter2/binary/DoubleEx.java
@@ -0,0 +1,19 @@
+package binary;
+
+public class DoubleEx {
+	
+	int abc;
+
+	public static void main(String[] args) {
+		
+		var dNum = 3.14;
+		float fNum	= 3.14F;
+		
+		System.out.println(dNum);
+		
+		var num = 10;
+		System.out.println(num);
+		
+	}
+
+}
