8aae2e718cae3af294b9a0355d08d0fd9f7f33fc
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2022-01-31 22:25:00 +0900

    Add files via upload

---
+++ JAVA/study/Basic/Chapter2/binary/VariableEx.java
@@ -0,0 +1,20 @@
+package binary;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		
+		int num;
+		num = 10;
+		
+		System.out.println(num);
+		
+		int level = 20;
+		System.out.println(level);
+		
+		// 변수를 선언할 때 이름에 의미를 부여. 가독성있게. 밑에 비교해보기.
+		int ns;
+		int numverOfStudent;
+	}
+
+}
