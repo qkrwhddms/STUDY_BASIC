f55ffb245b7a4ab4e07a504743e135ebeb1d88ad
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2022-02-08 23:27:32 +0900

    Add files via upload

---
+++ JAVA/study/Basic/Chapter3/assignment/OperationEx4.java
@@ -0,0 +1,30 @@
+package assignment;
+
+public class OperationEx4 {
+
+	public static void main(String[] args) {
+
+	/*	int num1 = 10;
+		System.out.println(num1 += 1);
+		
+		System.out.println(num1 %= 10);		// 몫(/=), 나머지(%=)
+		num1 -= 1;	// num1 = num1 - 1;
+		System.out.println(num1);
+		*/
+		
+	/*	int num = (5 > 3)? 10:20;	// true이면 10, false이면 20 (문장 가능)
+		System.out.println(num);
+		*/
+		
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch;
+		ch = (fatherAge > motherAge) ? 'T' : 'F';
+		
+		System.out.println(ch);
+	
+	
+	}
+
+}
