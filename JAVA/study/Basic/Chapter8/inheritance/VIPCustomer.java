9e5a71353e0d245e1a3d2867266235d26ae76829
qkrwhddms <64997375+qkrwhddms@users.noreply.github.com>
2022-04-07 10:56:14 +0900

    Add files via upload

---
+++ JAVA/study/Basic/Chapter8/inheritance/VIPCustomer.java
@@ -0,0 +1,31 @@
+package inheritance;
+
+public class VIPCustomer extends Customer{
+	
+	private double saleRatio;
+	private int agentID;
+	
+	
+	
+	public VIPCustomer(int customerID, String customerName, int agentID){
+		super(customerID, customerName);
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		saleRatio = 0.1;
+		this.agentID = agentID;
+		
+	//	System.out.println("VIPCusomer(int, String) 생성자 호출");
+	}
+	
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price * saleRatio);
+	}
+	
+
+	public int getAgentID() {
+		return agentID;
+	}
+	
+
+}
